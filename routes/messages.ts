d3f7c380854f23ad60795e3b7888b2a6c1669632
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-06 16:35:04 +0600

    Создан структура POST запросов и вовода ответа

--- routes/messages.ts
+++ routes/messages.ts
@@ -1,5 +1,23 @@
 import express from "express";
+import {promises as fs} from "fs";
+import {Message} from "../types";
 
 const messagesRouter = express.Router();
+const path = './messages';
+
+messagesRouter.post('/', async (req, res) => {
+    const newMessage: Message = {
+        message: req.body.message,
+        datetime: new Date().toISOString(),
+    };
+
+    try {
+        await fs.writeFile(`${path}/${newMessage.datetime}.txt`, JSON.stringify(newMessage));
+    } catch (e) {
+        console.error(e);
+    }
+
+    return res.send(newMessage);
+});
 
 export default messagesRouter;
